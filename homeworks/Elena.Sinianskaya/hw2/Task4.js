884c86ebd02c86b88c5436774af8607b72a28488
ElenaSinianskaya <Elena.Sinianskaya@gmail.com>
2018-04-26 17:46:41 +0300

    Add extra varian to task 4

--- homeworks/Elena.Sinianskaya/hw2/Task4.js
+++ homeworks/Elena.Sinianskaya/hw2/Task4.js
@@ -3,23 +3,35 @@
  * @param {number} second
  * @returns {boolean} Returns devide first on second without remainder or not.
  */
+// Функция принимает количество часов (проверить что это число).
+// Функция возвращает количество секунд в переданых часах.
+//  _____start_____ Вариант-1
+// function isDevided(first, second) {
+//   if (typeof first === "number" && typeof second === "number"){
+//      if (first%second === 0){
+//        return true;
+//     }
+//     else {
+//        return false;
+//     }
+//   }
+//   else {
+//     return("Not a number! Please, wright a number");
+//   }
+// }
+//  _____finish_____
 
-//  _____start_____
+// Вариант-2
 function isDevided(first, second) {
-  if (typeof first === "number" && typeof second === "number"){
-     if (first%second === 0){
-       return true;
+  if (typeof first === "number" && typeof second === "number" && first%second === 0 ){
+        return true;
     }
     else {
        return false;
     }
   }
-  else {
-    return("Not a number! Please, wright a number");
-  }
-}
-console.log(isDevided(10, 2));
+ 
+console.log(isDevided(11, 2));
 
-//   finish
 
 module.exports = isDevided;
