29b6e59da7b5c4a2cb5e247f675f14c1a1350d2c
ElenaSinianskaya <Elena.Sinianskaya@gmail.com>
2018-04-26 17:39:05 +0300

    Add extra variants to task 1 and task -2

--- homeworks/Elena.Sinianskaya/hw2/Task1.js
+++ homeworks/Elena.Sinianskaya/hw2/Task1.js
@@ -5,25 +5,50 @@
  */
 
 //  task_1
-// сделала не от 1, а от 0:
+// вариант с 0:
+// function stringBuilder(n) {
+//   if (typeof n === "number"){
+//     var i;
+//     var strPositive = 0; //- если ничего не присвоить переменной сейчас, то в возвращаемой строке будет в начале слово undefined.
+//     var strNegative = 0;
+//     // возвращает строку, если n>=0 от 0 до n (включительно, т.к. указали, что i<=n ).
+//     if (n>=1){
+//        for (i=1; i<=n; i++){
+//          strPositive +="," + i ;
+//        }
+//        return strPositive; //ставим тут, а не в середине, т.к. return прекращает функцию.
+//     }
+//     // возвращает строку от 0 до -n.
+//     else if (n<=1){
+//               for (i=-1; i>=n; i--){//i=-1 - чтоб строка формировалась последовательно. А i>=n - чтоб и n возвращало включительно.
+//          strNegative += ',' + i ;
+//        }
+//        return strNegative;
+//     }
+//   }
+//     else{
+//      return ('Unfortunately it\'s not a number! Please, wright a number.' );
+//    }
+// }
+// То же самое с 1:
 function stringBuilder(n) {
   if (typeof n === "number"){
     var i;
-    var strPositive = 0; //- если ничего не присвоить переменной сейчас, то в возвращаемой строке будет в начале слово undefined.
-    var strNegative = 0;
-    // возвращает строку, если n>=0 от 0 до n (включительно, т.к. указали, что i<=n ).
+    var strPositive = ''; 
+    var strNegative = '';
+   
     if (n>=1){
        for (i=1; i<=n; i++){
-         strPositive +="," + i ;
+         strPositive += i + ', ' ;
        }
-       return strPositive; //ставим тут, а не в середине, т.к. return прекращает функцию.
+       return strPositive.substring(0, strPositive.length - 2); //убираем 2 последних символа , и пробел
     }
-    // возвращает строку от 0 до -n.
+ 
     else if (n<=1){
-              for (i=-1; i>=n; i--){//i=-1 - чтоб строка формировалась последовательно. А i>=n - чтоб и n возвращало включительно.
-         strNegative += ',' + i ;
+              for (i=1; i>=n; i--){
+         strNegative +=  i + ', ' ;
        }
-       return strNegative;
+       return strNegative.substring(0, strNegative.length - 2);
     }
   }
     else{
--- homeworks/Elena.Sinianskaya/hw2/Task2.js
+++ homeworks/Elena.Sinianskaya/hw2/Task2.js
@@ -1,28 +1,56 @@
 /**
  * @param {number} minute
  * @returns {number} Returns the quoter based on input.
+ * 
+ * Задание 2. Функция принимает параметр min. 
+ * Параметр должен являться числом. В нем лежит 
+ * число от 0 до 59. Функция должна возвращать в 
+ * какую четверть часа попадает это число (в первую, 
+ * вторую, третью или четвертую).
  */
 // function hourQuoter (minute) {
 //     return;
 // }
-// ____Start_____
-function hourQuoter (minute) {
-  if (typeof minute === "number") {
-     if (minute >=0 && minute <=14){
-       return ("first quarter");
-     } else if (minute >=15 && minute <=29){
-       return ("second quarter");
-     } else if (minute >=30 && minute <=44){
-       return ("third quarter");
-     } else if (minute >=45 && minute <=60){
-       return ("fourth quarter");
-     }
-   }
- else{
-   return ("Not appropriate, wright another amount!");
- }
-}
-console.log(hourQuoter (22));
+// ____Start_____ Вариант -1 с if
+// function hourQuoter (minute) {
+//   if (typeof minute === "number") {
+//      if (minute >=0 && minute <=14){
+//        return ("first quarter");
+//      } else if (minute >=15 && minute <=29){
+//        return ("second quarter");
+//      } else if (minute >=30 && minute <=44){
+//        return ("third quarter");
+//      } else if (minute >=45 && minute <=60){
+//        return ("fourth quarter");
+//      }
+//    }
+//  else{
+//    return ("Not appropriate, wright another amount!");
+//  }
+// }
+// console.log(hourQuoter (22));
 // ______end______ 
+
+// Вариант -2 со switch:
+function hourQuoter (minute) {
+  switch (typeof minute === "number"){
+    case (minute >=0 && minute <=14):
+      return ("first quarter");
+      // break; - нельзя break после return, т.к. ф-ция и так заканчивается.
+    case (minute >=15 && minute <=29):
+      return ("second quarter");
+      // break;
+    case (minute >=30 && minute <=44):
+      return ("third quarter");
+      // break;
+    case (minute >=45 && minute <=60):
+      return ("fourth quarter");
+      // break;
+    default:
+      return ("Not appropriate, wright another amount!");
+  }
+}
+
+console.log(hourQuoter(46))
 
 module.exports = hourQuoter;
