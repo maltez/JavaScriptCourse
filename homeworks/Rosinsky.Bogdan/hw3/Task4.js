a36a40ef10433c302fe8947ca0c4d1ca00e6d68b
Bogdan Rosinsky <mancer.br@gmail.com>
2018-04-29 20:59:10 +0300

    add task4

--- homeworks/Rosinsky.Bogdan/hw3/Task4.js
+++ homeworks/Rosinsky.Bogdan/hw3/Task4.js
@@ -4,8 +4,42 @@
  * @param {string} key key for encoding
  * @returns {Array<number>} Returns encoded array of numbers.
  */
-function encode(input, key) {
-    return;
+// Getting the digit code.
+function getCode(input, key, len, i) {
+    for (let j = 0; j < len; j += 1) {
+        if (input[i] === key[j]) {
+            return j + 1;
+        }
+    }
+    return false;
+}
+// Getting the letter code.
+function getLetter(codeArr, key, len, i) {
+    for (let j = 0; j < len; j += 1) {
+        if ((j + 1) === codeArr[i]) {
+            return key[j];
+        }
+    }
+    return false;
+}
+
+function encode(input, key = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя ') {
+    // If not string then exit.
+    if (typeof input !== 'string' || typeof key !== 'string') {
+        return;
+    }
+    // Small letters.
+    const inputLowerCase = input.toLowerCase();
+    const keyLowerCase = key.toLowerCase();
+    // Length of string and key.
+    const inputlen = input.length;
+    const keyLen = key.length;
+    const lettersForCoding = [];
+
+    for (let i = 0; i < inputlen; i += 1) {
+        lettersForCoding[i] = getCode(inputLowerCase, keyLowerCase, keyLen, i);
+    }
+    return lettersForCoding;
 }
 
 /**
@@ -14,8 +48,20 @@ function encode(input, key) {
  * @param {string} key key for decoding
  * @returns {string} Returns decoded string.
  */
-function decode(input, key) {
-    return;
+function decode(input = [], key = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя ') {
+    // If the first argument is not an array and the second is not a string, then exit.
+    if (!Array.isArray(input) || typeof key !== 'string') {
+        return false;
+    }
+
+    let letters = '';
+    const keyLowerCase = key.toLowerCase();
+    const keyLength = key.length;
+
+    for (let i = 0; i < input.length; i += 1) {
+        letters += getLetter(input, keyLowerCase, keyLength, i);
+    }
+    return letters;
 }
 
 module.exports = {
