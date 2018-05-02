d118d59ae533dc753552ac4af3d8adcfcaddeeff
Bogdan Rosinsky <mancer.br@gmail.com>
2018-05-02 08:59:44 +0300

    corrected the comment

--- homeworks/Rosinsky.Bogdan/hw3/Task4.js
+++ homeworks/Rosinsky.Bogdan/hw3/Task4.js
@@ -1,8 +1,8 @@
 /**
- * Decode array of numbers to string by Stirlitz method
- * @param {Array<number>} input Array of numbers
- * @param {string} key key for decoding
- * @returns {string} Returns decoded string.
+ * Encode string into number array by Stirlitz method
+ * @param {string} input string for encoding
+ * @param {string} key key for encoding
+ * @returns {Array<number>} Returns encoded array of numbers.
  */
 function encode(input, key = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя ') {
     // If not string then exit.
