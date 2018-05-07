a1e29c302c94252589ee1bee4d8010c3aecc9b99
BahrovaSasha <38971137+BahrovaSasha@users.noreply.github.com>
2018-05-07 13:59:11 +0300

    Create task4.js

---
+++ homeworks/Sasha.Bahrova/hw2/task4.js
@@ -0,0 +1,16 @@
+@ -0,0 +1,8 @@
+/**
+ * @param {number} first
+ * @param {number} second
+ * @returns {boolean} Returns devide first on second without remainder or not.
+ */
+function isDevided(first, second) {
+ var result = first%second;
+ if(result === 0){
+   return true;
+ }else{
+   return false;
+ }
+}
+
+module.exports = isDevided;
