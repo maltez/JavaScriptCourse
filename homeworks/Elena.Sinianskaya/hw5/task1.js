2b24713119d15ecc0a25bd0add3f5d61c8d8535a
ElenaSinianskaya <Elena.Sinianskaya@gmail.com>
2018-05-13 10:16:48 +0300

    Add some changes to task 1

---
+++ homeworks/Elena.Sinianskaya/hw5/task1.js
@@ -0,0 +1,22 @@
+/**
+ * String encription
+ * @param {string} text
+ * @param {number} n
+ * @returns {string} Returns Encrypted string.
+ */
+
+ const encrypt = (text, n) => {
+     return
+ };
+
+/**
+ * String encription
+ * @param {string} text
+ * @param {number} n
+ * @returns {string} Returns Decrypted string.
+ */
+const decrypt = (encryptedText, n) => {
+
+}
+
+module.exports = { encrypt , decrypt };
---
+++ homeworks/Elena.Sinianskaya/hw5/task2.js
@@ -0,0 +1,12 @@
+/**
+ * Compare of two arrays
+ * @param {Array<number>} num1
+ * @param {Array<number>} num2
+ * @returns {boolean} Compares arrays or not.
+ */
+
+const comp = (array1, array2) => {
+    //your code here
+}
+
+module.exports = comp;
