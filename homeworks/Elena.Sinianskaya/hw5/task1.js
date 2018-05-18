85b29ebb576d58bbfd3b2b4c97a33a9144a98109
ElenaSinianskaya <Elena.Sinianskaya@gmail.com>
2018-05-18 14:04:18 +0300

    Add first task

--- homeworks/Elena.Sinianskaya/hw5/task1.js
+++ homeworks/Elena.Sinianskaya/hw5/task1.js
@@ -6,22 +6,45 @@
  */
 
 const encrypt = function (text, n){
-    if (typeof text !== 'string' || !text){
-      return "This is a test!"
-    }
-    let textArr = text.split('');
-    let determineEven = function(item, i){
-        return (i%2==0)
-      };
-    let determineUneven = function(item, i){
-        return (i%2!==0)
-    };
-    
-     for (let i = 0; i < n; i+=1){
-        textArr = textArr.filter(determineUneven).concat(textArr.filter(determineEven))
-    }
-    return textArr.join('')
+  if (typeof text !== 'string' || !text){
+    return "This is a test!"
   }
+  let textArr = text.split('');
+  let determineEven = function(item, i){
+      return (i%2==0)
+    };
+  let determineUneven = function(item, i){
+      return (i%2!==0)
+  };
+  
+   for (let i = 0; i < n; i+=1){
+      textArr = textArr.filter(determineUneven).concat(textArr.filter(determineEven))
+  }
+  return textArr.join('')
+}
+
+const decrypt = function (encryptedText, n){
+  if (typeof encryptedText !== 'string' || !encryptedText) {
+       return encryptedText;
+   }
+  
+  function fn() {
+   let arrText = encryptedText.split('');
+   let firstPart = arrText.slice (0, (arrText.length/2));
+   let secondPart = arrText.slice ((arrText.length/2), (arrText.length));
+   let finalArray = [];
+   for(let h = 0; h<(firstPart.length)+1; h+=1){
+     finalArray.push(secondPart[h], firstPart[h])//запушить в новый массив элементы через один (один элемент с одного массива, другой - с другого)
+   }
+  
+   return finalArray
+   }
+ let getArr =  fn()
+ return  getArr.join('')
+}
+
+
+
 
 
 /**
