79769e498cb9fe9e6962c402cb3f8b9177865a5e
Slavik <slavpas@gmail.com>
2018-09-28 08:59:20 +0300

    Completed 2nd homework

---
+++ homeworks/Viacheslav.Pastushenko/hw2/Task5.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @returns {number} Returns factorial of num.
+ */
+function factorial(num) {
+    if (typeof num == 'number' && num > 0) {
+        var getFactorial = 1;
+        for (var counter = 1; counter <= num; counter += 1) {
+            getFactorial *= counter;
+        }
+        return getFactorial;
+    } else {
+        return false;
+    }
+}
+
+module.exports = factorial;
