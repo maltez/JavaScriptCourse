6e5b98cdb7d43422af32806c5477f763597a43c0
Kyryl Shekhanin <kyryl.shekhanin@nure.ua>
2018-09-29 11:13:02 +0300

    Start make hw2

---
+++ homeworks/Kyryl.Shekhanin/hw2/Task1.js
@@ -0,0 +1,13 @@
+/**
+ * Build string from numbers from 1 to n
+ * @param {number} input
+ * @returns {string} Returns the string from 1 to input.
+ */
+function stringBuilder(input) {
+
+    if(typeof input !== 'nuber')
+        return;
+};
+
+module.exports = stringBuilder;
+
