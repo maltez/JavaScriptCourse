9c24869244ce57787b3ca56e11da99da1fe21726
RkodR <ruslakod@gmail.com>
2018-09-29 17:09:11 +0300

    homeworks

---
+++ homeworks/Ruslan.Kodrul/hw2/task5.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} num
+ * @returns {number} Returns factorial of num.
+ */
+function factorial(num) {
+
+    if ( typeof num !== 'number'){
+        return false;
+    }
+
+    var composition = 1;
+
+    if ( num > 0){
+        for ( var i = 1; i < num; i++){
+            composition *= i;
+        }
+        return composition;
+    } else{
+        return false;
+    }
+    
+}
+
+module.exports = factorial;
