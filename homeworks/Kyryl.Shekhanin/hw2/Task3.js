c4d264d23fadea1116658c51383e687e4f861c1c
Kyryl Shekhanin <kyryl.shekhanin@nure.ua>
2018-10-01 00:22:07 +0300

    Made task3

--- homeworks/Kyryl.Shekhanin/hw2/Task3.js
+++ homeworks/Kyryl.Shekhanin/hw2/Task3.js
@@ -3,7 +3,13 @@
  * @returns {number} Returns count of seconds in input hours.
  */
 function secondsCount(hours) {
-    return;
+    if ((typeof hours) !== 'number') {
+        return;
+    }
+    var minute = hours * 60;
+    var seconds = minute * 60;
+    return seconds;
 }
+
 
 module.exports = secondsCount;
