22ca49a9777a6a30a848c084040d4df0e4669efc
Kyryl Shekhanin <kyryl.shekhanin@nure.ua>
2018-10-01 00:40:41 +0300

    Made task5

--- homeworks/Kyryl.Shekhanin/hw2/Task5.js
+++ homeworks/Kyryl.Shekhanin/hw2/Task5.js
@@ -3,7 +3,14 @@
  * @returns {number} Returns factorial of num.
  */
 function factorial(num) {
-    return;
+    if ((typeof num) !== 'number' || num < 0) {
+        return;
+    }
+    var factor = 1;
+    for (num; num > 1; num -= 1) {
+        factor *= num;
+    }
+    return factor;
 }
 
 module.exports = factorial;
