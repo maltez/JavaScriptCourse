cef66fe8e952f93eb6a505f42cf6c405222908b7
Kyryl Shekhanin <kyryl.shekhanin@nure.ua>
2018-10-01 00:29:58 +0300

    Made task 4

--- homeworks/Kyryl.Shekhanin/hw2/Task4.js
+++ homeworks/Kyryl.Shekhanin/hw2/Task4.js
@@ -4,7 +4,10 @@
  * @returns {boolean} Returns devide first on second without remainder or not.
  */
 function isDevided(first, second) {
-    return;
+    if ((typeof first) !== 'number' && (typeof second) !== 'number') {
+        return;
+    }
+    return !(first % second);
 }
 
 module.exports = isDevided;
