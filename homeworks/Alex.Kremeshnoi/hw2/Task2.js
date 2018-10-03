917ec34e7ec270660ec4e879c2fcf9d9e6683bb8
Alexander Kremeshnoi <alex.kremeshnoi@gmail.com>
2018-10-03 23:53:29 +0300

    completed Task.2js and Task4.js

--- homeworks/Alex.Kremeshnoi/hw2/Task2.js
+++ homeworks/Alex.Kremeshnoi/hw2/Task2.js
@@ -3,7 +3,19 @@
  * @returns {number} Returns the quoter based on input.
  */
 function hourQuoter (minute) {
-    return;
+    if (typeof minute === 'number') {
+        if (minute <= 15) {
+            return 'first quarter'
+        } else if (minute <= 30) {
+            return 'second quarter'
+        } else if (minute <= 45) {
+            return 'third quarter'
+        } else if (minute <=59) {
+            return 'fourth quarter'
+        } else {
+            return false
+        }
+    }
 }
 
 module.exports = hourQuoter;
--- homeworks/Alex.Kremeshnoi/hw2/Task4.js
+++ homeworks/Alex.Kremeshnoi/hw2/Task4.js
@@ -4,7 +4,12 @@
  * @returns {boolean} Returns devide first on second without remainder or not.
  */
 function isDevided(first, second) {
-    return;
+    if (first % second == 0) {
+        return true 
+    } else {
+        return false
+    }
 }
 
 module.exports = isDevided;
+
