417d15de47fadded436c78743faa4f8c2dfd254f
Kyryl Shekhanin <kyryl.shekhanin@nure.ua>
2018-10-05 12:30:29 +0300

    Made Task2

--- homeworks/Kyryl.Shekhanin/hw3/Task2.js
+++ homeworks/Kyryl.Shekhanin/hw3/Task2.js
@@ -4,7 +4,16 @@
  * @returns {Array<number>} Returns filtered array.
  */
 function advancedFilter(input) {
-    return;
+    var ResultArray = [];
+    for (let i = 0; i < input.length; i += 1) {
+        if (typeof input[i] === 'number') {
+            if (input[i] > 0 && input[i] < 10) {
+                ResultArray.push(input[i]);
+            }
+        }
+    }
+    return ResultArray;
 }
 
+console.log(advancedFilter([1, 2, 'her', -2, 100, 9, 10]));
 module.exports = advancedFilter;
