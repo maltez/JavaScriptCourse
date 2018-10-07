4d40d41c6e176c0946923ebcae3e6b82249470fe
Kyryl Shekhanin <kyryl.shekhanin@nure.ua>
2018-10-07 20:21:03 +0300

    Update homeworks/Kyryl.Shekhanin/hw4/task5.js

--- homeworks/Kyryl.Shekhanin/hw4/task5.js
+++ homeworks/Kyryl.Shekhanin/hw4/task5.js
@@ -4,7 +4,24 @@
  * @returns {Array<number>} Returns filtered array.
  */
 const moveZeros = function (arr) {
-    return
+    var ZeroArray = [];
+    var ResultArray = [];
+
+    for (var i = 0; i < arr.length; i += 1) {
+        if (arr[i] !== 0) {
+            ResultArray.push(arr[i]);
+        } else {
+            ZeroArray.push(arr[i]);
+        }
+    }
+
+    for (var i = 0; i < ZeroArray.length; i += 1) {
+        ResultArray.push(ZeroArray[i]);
+    }
+
+    return ResultArray;
 }
 
+var arr = [11, 42, 0, 0, 52, 2, 0, 1, 3, 5, 21, 25]
+console.log(moveZeros(arr));
 module.exports = moveZeros;
