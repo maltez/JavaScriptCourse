02a17010bbcb521b57a7fa2462c305805082cbe5
Kyryl Shekhanin <kyryl.shekhanin@nure.ua>
2018-10-07 19:04:12 +0300

    Update homeworks/Kyryl.Shekhanin/hw4/task4.js

--- homeworks/Kyryl.Shekhanin/hw4/task4.js
+++ homeworks/Kyryl.Shekhanin/hw4/task4.js
@@ -5,7 +5,19 @@
  * @returns {number} Returns filtered array.
  */
 const maxMin = (input, order = true) => {
-    return;
+   let Order;
+
+    for (var i = 0; i < input.length; i += 1) {
+        if (order) {
+            Order = Order > input[i] ? Order : input[i]
+        } else {
+            Order = Order <= input[i] ? Order : input[i]
+        }
+    }
+
+    return Order;
 };
+
+console.log(maxMin([11, 42, 52, 2, 1, 3, 5, 21, 25],0));
 
 module.exports = maxMin;
