65b3bc85b55319231a6bd7cf72af6b20bcc3e510
Anton Kot <kotov977@gmail.com>
2018-10-16 19:45:21 +0300

    some fix in methods

--- homeworks/Anton.Pavlenko/hw4/task4.js
+++ homeworks/Anton.Pavlenko/hw4/task4.js
@@ -7,10 +7,24 @@
 const maxMin = (input, order = false) => {
 
     if(order){
-    return Math.max.apply(null,input);
+        var max = input[0];
+        for (var i = 0; i < input.length; i++) {
+            if (max < input[i]) {
+                max = input[i];
+            } 
+        }
+        return max;
     }
-    if(!order)
-    return Math.min.apply(null,input);
+    if(!order){
+        var min = input[0];
+        for (var i = 0; i < input.length; i++) {
+            if (min > input[i]){
+                min = input[i];
+            }
+        }
+        return min;
+    }
+    
 };
 
 var arrNum=[3,-6,34,7,23,25,493,56,8,5,335];
