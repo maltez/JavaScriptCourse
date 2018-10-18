b6e86219b01832b772fd6955f682b81ae483efff
Anton Kot <43553139+pavlzw86@users.noreply.github.com>
2018-10-18 10:53:45 +0300

    remove comments

--- homeworks/Anton.Pavlenko/hw3/Task3.js
+++ homeworks/Anton.Pavlenko/hw3/Task3.js
@@ -3,9 +3,7 @@
  * @param {string} input
  * @returns {boolean} Returns is string polindroe or not.
  */
-// В википедии прочитать что такое полиндромы. 
-// Функция должна принимать строку и выдавать true если строка является полндромом
-//  и false если она таковой не является. Игнорировать большие и малые буквы.
+
 var str = 'english';
 var str1 = 'level';
 
