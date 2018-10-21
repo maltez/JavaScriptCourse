ee60bf4a7f62ddcc2400e313bfae24b427cf07ce
Alexander Kremeshnoi <alex.kremeshnoi@gmail.com>
2018-10-21 21:33:45 +0300

    all tasks were solved

---
+++ homeworks/Alex.Kremeshnoi/hw4/task5.js
@@ -0,0 +1,10 @@
+/**
+ * Filter array. Move zeros in the end of string.
+ * @param {Array<number>} input
+ * @returns {Array<number>} Returns filtered array.
+ */
+const moveZeros = function (arr) {
+    return
+}
+
+module.exports = moveZeros;
