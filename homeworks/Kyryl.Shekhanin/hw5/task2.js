6568773c5a48b94aa887f9bd58fab653346dda2e
Kyryl Shekhanin <kyryl.shekhanin@nure.ua>
2018-10-23 02:00:35 +0300

    Made 2nd Task

--- homeworks/Kyryl.Shekhanin/hw5/task2.js
+++ homeworks/Kyryl.Shekhanin/hw5/task2.js
@@ -6,7 +6,36 @@
  */
 
 const comp = (array1, array2) => {
-    //your code here
+
+    if (!array1 || !array2) {
+        return false;
+    }
+    else if (!array1.length || !array2.length) {
+        return false;
+    }
+
+
+    var isSquare = false;
+    var squareValue;
+
+    for (var i2 = 0; i2 < array2.length; i2 += 1) {
+        isSquare = false;
+        for (var i1 = 0; i1 < array1.length; i1 += 1) {
+            squareValue = array1[i1] * array1[i1];
+            if (array2[i2] === squareValue) {
+                isSquare = true;
+                break;
+            }
+        }
+        if (!isSquare) {
+            return isSquare;
+        }
+    }
+
+    return isSquare;
 }
 
 module.exports = comp;
+
+
+console.log(comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]));
