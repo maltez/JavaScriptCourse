120a7a61a09af4e7aae47ea268d9610b3dd8e809
Alexander Kremeshnoi <alex.kremeshnoi@gmail.com>
2018-11-02 18:41:52 +0200

    task2.js solved

--- homeworks/Alex.Kremeshnoi/hw5/task2.js
+++ homeworks/Alex.Kremeshnoi/hw5/task2.js
@@ -6,7 +6,23 @@
  */
 
 const comp = (array1, array2) => {
-    //your code here
-}
+    for (let i = 0; i < array2.length; i +=1) {
+        var sqrt;
+        var bool = false;
+        for(let j = 0; j < array1.length; j +=1) {
+            sqrt = array1[j]**2;
+            console.log(sqrt)
+            if (sqrt === array2[i]) {
+                bool = true;
+                break;
+            }
+        }
+        if (!bool) {
+            return false
+        }
+    } return true
+} 
+
+console.log(comp([121, 144, 19, 161, 19, 144, 19, 11],[121, 14641, 20736, 361, 25921, 361, 20736, 361]));
 
 module.exports = comp;
