5aa3173c63d2acf552eb477a7fe3eba5662c3d7f
Pavel Arseyev <pasha788b@gmail.com>
2019-01-16 01:03:41 +0200

    finished 5th task of the homework

--- homeworks/Pavel.Arseyev/hw2/Task5.js
+++ homeworks/Pavel.Arseyev/hw2/Task5.js
@@ -2,8 +2,28 @@
  * @param {number} num
  * @returns {number} Returns factorial of num.
  */
+//мой вариант
 function factorial(num) {
-    return;
+    if(typeof num !== 'number' || num < 1 || !num){
+        return 'Invalid. Please ansert a value more or equal to 1';
+    }
+    let result = num;
+    for(let i = num; i > 1; i--){
+        result *= i-1;
+    }
+    return result;
 }
+
+console.log(factorial(5));
+
+//Второй вариант(подсмотрел на learn.javascript.ru)
+function fact(n){
+    if(typeof n !== 'number' || n < 1 || !n){
+        return 'Invalid. Please ansert a value more or equal to 1';
+    }
+    return (n != 1) ? n * fact(n-1) : 1;
+}
+
+console.log(fact(5));
 
 module.exports = factorial;
