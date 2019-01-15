758740c117d99cea49676e670ff2d3de9a1324e7
AlexeiDy <alexei.dyachek@gmail.com>
2019-01-15 11:21:58 +0200

    Task5 is Done

---
+++ homeworks/Alexei.Diachek/hw2/Task5.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @returns {number} Returns factorial of num.
+ */
+function factorial(num) {
+  if (typeof num !== 'number') {
+    return false;
+  }
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+module.exports = factorial;
