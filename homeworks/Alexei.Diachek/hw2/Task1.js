c021baa4f470da25b9b10fa8f067a095e7af6b57
AlexeiDy <alexei.dyachek@gmail.com>
2019-01-15 11:20:57 +0200

    Task1 is done

---
+++ homeworks/Alexei.Diachek/hw2/Task1.js
@@ -0,0 +1,28 @@
+/**
+ * Build string from numbers from 1 to n
+ * @param {number} input
+ * @returns {string} Returns the string from 1 to input.
+ */
+function stringBuilder(input) {
+  // Output starts from startPoint
+  let startPoint = 1;
+  let result = '' + startPoint;
+
+  if (typeof input !== 'number') {
+    return false;
+  }
+
+  if (input > 1) {
+    while (startPoint < input) {
+      startPoint += 1;
+      result += ', ' + startPoint;
+    }
+  } else {
+    while (startPoint > input) {
+      startPoint -= 1;
+      result += ', ' + startPoint;
+    }
+  }
+  return result;
+}
+module.exports = stringBuilder;
