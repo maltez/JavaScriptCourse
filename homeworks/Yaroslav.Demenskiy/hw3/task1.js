9510bd53ed5c688ea43e7af8286c261f777f312e
Yaroslav Demenskiy <Demenskiy@MacBook-Pro-Yaroslav.local>
2019-01-19 18:22:52 +0200

    Homework 3 is completed 3/4

---
+++ homeworks/Yaroslav.Demenskiy/hw3/task1.js
@@ -0,0 +1,30 @@
+/**
+ * Filter negative numbers from array
+ * @param {Array<number>} input
+ * @returns {Array<number>} Returns array that contains positive numbers only.
+ */
+function negativeFilter(input) {
+
+    let positiveNumbers = []; 
+
+    for (let i = 0; i < input.length; i++) {
+        if (isPositive(input[i])) {
+            positiveNumbers += input[i];
+        }
+    }
+
+    if (positiveNumbers.length > 0) {
+        return positiveNumbers;
+    } else {
+        return 'No matching numbers.';
+    }
+
+}
+
+function isPositive(number) {
+    return number > 0 ? true : false;
+}
+
+console.log(negativeFilter([-1, 1, -2, 2, -3, 3]));
+
+module.exports = negativeFilter;
