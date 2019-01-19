a1f4a56f5484ba72405e4b84beaaccb3551fc5b9
AlexeiDy <alexei.dyachek@gmail.com>
2019-01-19 19:05:46 +0200

    Added second variants of functions

--- homeworks/Alexei.Diachek/hw3/Task4.js
+++ homeworks/Alexei.Diachek/hw3/Task4.js
@@ -5,6 +5,130 @@
  * @returns {Array<number>} Returns encoded array of numbers.
  */
 function encode(input, key = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя') {
+  if (typeof input !== 'string') {
+    return false;
+  }
+  if (key === 'js') {
+    let alphabet = [
+      'а',
+      'б',
+      'в',
+      'г',
+      'д',
+      'е',
+      'ё',
+      'ж',
+      'з',
+      'и',
+      'й',
+      'к',
+      'л',
+      'м',
+      'н',
+      'о',
+      'п',
+      'р',
+      'с',
+      'т',
+      'у',
+      'ф',
+      'х',
+      'ц',
+      'ч',
+      'ш',
+      'щ',
+      'ъ',
+      'ы',
+      'ь',
+      'э',
+      'ю',
+      'я',
+      '.',
+      ',',
+      ' '
+    ];
+    let result = [];
+    input
+      .toLowerCase()
+      .split('')
+      .forEach(function(e) {
+        if (alphabet.indexOf(e) !== -1) {
+          result.push(alphabet.indexOf(e));
+        }
+      });
+    return result;
+  } else {
+    return 'Your key is wrong';
+  }
+}
+/**
+ * Decode array of numbers to string by Stirlitz method
+ * @param {Array<number>} input Array of numbers
+ * @param {string} key key for decoding
+ * @returns {string} Returns decoded string.
+ */
+function decode(input, key = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя') {
+  if (!Array.isArray(input)) {
+    return false;
+  }
+  if ((key = 'js')) {
+    let output = '';
+    let alphabet = [
+      'а',
+      'б',
+      'в',
+      'г',
+      'д',
+      'е',
+      'ё',
+      'ж',
+      'з',
+      'и',
+      'й',
+      'к',
+      'л',
+      'м',
+      'н',
+      'о',
+      'п',
+      'р',
+      'с',
+      'т',
+      'у',
+      'ф',
+      'х',
+      'ц',
+      'ч',
+      'ш',
+      'щ',
+      'ъ',
+      'ы',
+      'ь',
+      'э',
+      'ю',
+      'я',
+      '.',
+      ',',
+      ' '
+    ];
+    input.forEach(function(e) {
+      if (e >= 0 && e < alphabet.length) {
+        output += alphabet[e];
+      }
+    });
+    return output;
+  } else {
+    return 'Your key is wrong';
+  }
+}
+
+/**
+ * Encode string into number array by Stirlitz method
+ * @param {string} input string for encoding
+ * @param {string} key key for encoding
+ * @returns {Array<number>} Returns encoded array of numbers.
+ */
+function encode_v2(input, key = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя') {
   if (key === 'js') {
     let arrOfCodes = [];
     for (var i = 0; i < input.length; i++) {
@@ -22,7 +146,7 @@ function encode(input, key = 'абвгдеёжзийклмнопрстуфхцч
  * @param {string} key key for decoding
  * @returns {string} Returns decoded string.
  */
-function decode(input, key = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя') {
+function decode_v2(input, key = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя') {
   if (key === 'js') {
     let arrOfLetters = [];
     input.forEach(function(e) {
