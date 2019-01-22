3446af73366d6ba8005cd98dfae28dec395ff01c
AlexeiDy <alexei.dyachek@gmail.com>
2019-01-22 12:21:44 +0200

    hw4/task2.js is done

---
+++ homeworks/Alexei.Diachek/hw4/task2.js
@@ -0,0 +1,22 @@
+/**
+ * Calculates Lowest Common Multiple
+ * @param {number} num1
+ * @param {number} num2
+ * @returns {number} Returns the Lowest Common Multiple.
+ */
+
+const lowestMultiple = (num1, num2) => {
+  let minNum = num1 > num2 ? num2 : num1;
+  let maxNum = num1 < num2 ? num2 : num1;
+  let count = minNum;
+  if (maxNum % minNum === 0) {
+    return maxNum;
+  }
+  while (true) {
+    if (count % maxNum === 0) {
+      return count;
+    }
+    count += minNum;
+  }
+};
+module.exports = lowestMultiple;
