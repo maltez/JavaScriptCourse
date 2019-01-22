c8dd183e4e3e555f026ffff528d404150c1b8ad4
AlexeiDy <alexei.dyachek@gmail.com>
2019-01-22 17:29:13 +0200

    added calculating start point for loop

--- homeworks/Alexei.Diachek/hw4/task2.js
+++ homeworks/Alexei.Diachek/hw4/task2.js
@@ -8,7 +8,8 @@
 const lowestMultiple = (num1, num2) => {
   let minNum = num1 > num2 ? num2 : num1;
   let maxNum = num1 < num2 ? num2 : num1;
-  let count = minNum;
+  //calculating start point for loop
+  let count = minNum * Math.floor(maxNum / minNum);
   if (maxNum % minNum === 0) {
     return maxNum;
   }
